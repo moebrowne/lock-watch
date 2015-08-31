da9034b1ca3cd2fae59aad37c937dc1e8b694bda
MoeBrowne <moebrowne@users.noreply.github.com>
2015-08-31 16:34:46 +0100

    Added a --system flag for selecting different systems to monitor

--- lock-watch.sh
+++ lock-watch.sh
@@ -10,6 +10,19 @@ PATH_ROOT="$DIR"
 # Set the scripts root path
 PATH_SCRIPTS_ROOT="$PATH_ROOT/scripts"
 
+# Pad the passed arguments with spaces for better REGEX detection
+args=" $@ "
+
+# Look for the
+regexSystemType=' --system (mate|gnome|unity) '
+[[ $args =~ $regexSystemType ]]
+if [ "${BASH_REMATCH[1]}" != "" ]; then
+	SYSTEM_TYPE="${BASH_REMATCH[1]}"
+else
+	SYSTEM_TYPE="mate"
+fi
+
+
 runLockScripts() {
 	run-parts --regex '\.sh$' "$PATH_SCRIPTS_ROOT/lock"
 }
@@ -19,7 +32,7 @@ runUnlockScripts() {
 }
 
 # Start monitoring for the lock and unlock events
-dbus-monitor --session "type='signal',interface='org.mate.ScreenSaver'" | \
+dbus-monitor --session "type='signal',interface='org.$SYSTEM_TYPE.ScreenSaver'" | \
 (
 while true; do
 	read X;
